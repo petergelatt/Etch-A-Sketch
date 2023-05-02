9e7fd98e93580fde2f745e7040a6f50cca217f45
petergelatt <petergelatt@gmail.com>
2023-05-01 20:25:58 -0400

    Size Selection Prompt
    Added a size selection function that begs a prompt which asks the user to provide and string response. That string is converted to a number and used as the argument in the "createGrid()" function.

--- myScript.js
+++ myScript.js
@@ -3,16 +3,28 @@
 // needs a loop that will continue to add new cells to the grid container until argument number is satisfied
 
 
- gridNumber = 16
+
+
+function setGridSize(){
+    let sizePrompt = prompt("Enter a number 1-64","");
+    let sizeInt = parseInt(sizePrompt);
+    gridNumber = sizeInt;
+    console.log(gridNumber)
+    createGrid(gridNumber);
+    }
 
 function createGrid(gridNumber) {
 
     let grid = document.getElementById('grid');
+    let cellSize = 500 / gridNumber; 
 
     for (i = 0; i < gridNumber * gridNumber; i++) {
         let cell = document.createElement('div');
-        cell.classList.add('cell'); // Add class 'cell' to each cell element
+        cell.classList.add('cell'); 
         cell.id = 'cell' + i;
+        cell.style.width = cellSize + 'px'; 
+        cell.style.height = cellSize + 'px';
+
         grid.appendChild(cell);
 
         cell.addEventListener('mouseenter', function() {
@@ -27,9 +39,13 @@ function createGrid(gridNumber) {
 
     
     }
+    grid.style.display = 'grid';
+    grid.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+    grid.style.gap = '0';
 }
 
-createGrid(16);
+
 
   
 
